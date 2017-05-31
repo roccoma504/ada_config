6897921e60314b9ce95a1d6ebe487893aeed16bc
Matt Rocco <roccoma504@gmail.com>
2017-05-30 21:31:02 -0400

    unit

---
+++ obj/b__main.ads
@@ -0,0 +1,192 @@
+pragma Ada_95;
+pragma Warnings (Off);
+with System;
+package ada_main is
+
+   gnat_argc : Integer;
+   gnat_argv : System.Address;
+   gnat_envp : System.Address;
+
+   pragma Import (C, gnat_argc);
+   pragma Import (C, gnat_argv);
+   pragma Import (C, gnat_envp);
+
+   gnat_exit_status : Integer;
+   pragma Import (C, gnat_exit_status);
+
+   GNAT_Version : constant String :=
+                    "GNAT Version: GPL 2016 (20160515-49)" & ASCII.NUL;
+   pragma Export (C, GNAT_Version, "__gnat_version");
+
+   Ada_Main_Program_Name : constant String := "_ada_main" & ASCII.NUL;
+   pragma Export (C, Ada_Main_Program_Name, "__gnat_ada_main_program_name");
+
+   procedure adainit;
+   pragma Export (C, adainit, "adainit");
+
+   procedure adafinal;
+   pragma Export (C, adafinal, "adafinal");
+
+   function main
+     (argc : Integer;
+      argv : System.Address;
+      envp : System.Address)
+      return Integer;
+   pragma Export (C, main, "main");
+
+   type Version_32 is mod 2 ** 32;
+   u00001 : constant Version_32 := 16#c8bac64d#;
+   pragma Export (C, u00001, "mainB");
+   u00002 : constant Version_32 := 16#b6df930e#;
+   pragma Export (C, u00002, "system__standard_libraryB");
+   u00003 : constant Version_32 := 16#337e9ce1#;
+   pragma Export (C, u00003, "system__standard_libraryS");
+   u00004 : constant Version_32 := 16#c3282aa7#;
+   pragma Export (C, u00004, "systemS");
+   u00005 : constant Version_32 := 16#a6359005#;
+   pragma Export (C, u00005, "system__memoryB");
+   u00006 : constant Version_32 := 16#9a554c93#;
+   pragma Export (C, u00006, "system__memoryS");
+   u00007 : constant Version_32 := 16#472fa979#;
+   pragma Export (C, u00007, "ada__exceptionsB");
+   u00008 : constant Version_32 := 16#a2017425#;
+   pragma Export (C, u00008, "ada__exceptionsS");
+   u00009 : constant Version_32 := 16#3ffc8e18#;
+   pragma Export (C, u00009, "adaS");
+   u00010 : constant Version_32 := 16#e947e6a9#;
+   pragma Export (C, u00010, "ada__exceptions__last_chance_handlerB");
+   u00011 : constant Version_32 := 16#41e5552e#;
+   pragma Export (C, u00011, "ada__exceptions__last_chance_handlerS");
+   u00012 : constant Version_32 := 16#5f84b5ab#;
+   pragma Export (C, u00012, "system__soft_linksB");
+   u00013 : constant Version_32 := 16#5dacf2f2#;
+   pragma Export (C, u00013, "system__soft_linksS");
+   u00014 : constant Version_32 := 16#b01dad17#;
+   pragma Export (C, u00014, "system__parametersB");
+   u00015 : constant Version_32 := 16#bd0227d8#;
+   pragma Export (C, u00015, "system__parametersS");
+   u00016 : constant Version_32 := 16#0f0cb66d#;
+   pragma Export (C, u00016, "system__secondary_stackB");
+   u00017 : constant Version_32 := 16#6849e5ce#;
+   pragma Export (C, u00017, "system__secondary_stackS");
+   u00018 : constant Version_32 := 16#39a03df9#;
+   pragma Export (C, u00018, "system__storage_elementsB");
+   u00019 : constant Version_32 := 16#eeeb60a3#;
+   pragma Export (C, u00019, "system__storage_elementsS");
+   u00020 : constant Version_32 := 16#41837d1e#;
+   pragma Export (C, u00020, "system__stack_checkingB");
+   u00021 : constant Version_32 := 16#4d97414f#;
+   pragma Export (C, u00021, "system__stack_checkingS");
+   u00022 : constant Version_32 := 16#87a448ff#;
+   pragma Export (C, u00022, "system__exception_tableB");
+   u00023 : constant Version_32 := 16#9e8643e5#;
+   pragma Export (C, u00023, "system__exception_tableS");
+   u00024 : constant Version_32 := 16#ce4af020#;
+   pragma Export (C, u00024, "system__exceptionsB");
+   u00025 : constant Version_32 := 16#ab4b4751#;
+   pragma Export (C, u00025, "system__exceptionsS");
+   u00026 : constant Version_32 := 16#4c9e814d#;
+   pragma Export (C, u00026, "system__exceptions__machineS");
+   u00027 : constant Version_32 := 16#aa0563fc#;
+   pragma Export (C, u00027, "system__exceptions_debugB");
+   u00028 : constant Version_32 := 16#bda2d363#;
+   pragma Export (C, u00028, "system__exceptions_debugS");
+   u00029 : constant Version_32 := 16#6c2f8802#;
+   pragma Export (C, u00029, "system__img_intB");
+   u00030 : constant Version_32 := 16#c1f3ca65#;
+   pragma Export (C, u00030, "system__img_intS");
+   u00031 : constant Version_32 := 16#39df8c17#;
+   pragma Export (C, u00031, "system__tracebackB");
+   u00032 : constant Version_32 := 16#9d0af463#;
+   pragma Export (C, u00032, "system__tracebackS");
+   u00033 : constant Version_32 := 16#9ed49525#;
+   pragma Export (C, u00033, "system__traceback_entriesB");
+   u00034 : constant Version_32 := 16#c373dcd7#;
+   pragma Export (C, u00034, "system__traceback_entriesS");
+   u00035 : constant Version_32 := 16#6fd210f2#;
+   pragma Export (C, u00035, "system__traceback__symbolicB");
+   u00036 : constant Version_32 := 16#dd19f67a#;
+   pragma Export (C, u00036, "system__traceback__symbolicS");
+   u00037 : constant Version_32 := 16#701f9d88#;
+   pragma Export (C, u00037, "ada__exceptions__tracebackB");
+   u00038 : constant Version_32 := 16#20245e75#;
+   pragma Export (C, u00038, "ada__exceptions__tracebackS");
+   u00039 : constant Version_32 := 16#57a37a42#;
+   pragma Export (C, u00039, "system__address_imageB");
+   u00040 : constant Version_32 := 16#62c4b79d#;
+   pragma Export (C, u00040, "system__address_imageS");
+   u00041 : constant Version_32 := 16#8c33a517#;
+   pragma Export (C, u00041, "system__wch_conB");
+   u00042 : constant Version_32 := 16#d8550875#;
+   pragma Export (C, u00042, "system__wch_conS");
+   u00043 : constant Version_32 := 16#9721e840#;
+   pragma Export (C, u00043, "system__wch_stwB");
+   u00044 : constant Version_32 := 16#f5442474#;
+   pragma Export (C, u00044, "system__wch_stwS");
+   u00045 : constant Version_32 := 16#a831679c#;
+   pragma Export (C, u00045, "system__wch_cnvB");
+   u00046 : constant Version_32 := 16#d7e2b286#;
+   pragma Export (C, u00046, "system__wch_cnvS");
+   u00047 : constant Version_32 := 16#5ab55268#;
+   pragma Export (C, u00047, "interfacesS");
+   u00048 : constant Version_32 := 16#ece6fdb6#;
+   pragma Export (C, u00048, "system__wch_jisB");
+   u00049 : constant Version_32 := 16#5792aba7#;
+   pragma Export (C, u00049, "system__wch_jisS");
+   u00050 : constant Version_32 := 16#b3b9fca9#;
+   pragma Export (C, u00050, "system__crtlS");
+   --  BEGIN ELABORATION ORDER
+   --  ada%s
+   --  interfaces%s
+   --  system%s
+   --  system.img_int%s
+   --  system.img_int%b
+   --  system.parameters%s
+   --  system.parameters%b
+   --  system.crtl%s
+   --  system.standard_library%s
+   --  system.exceptions_debug%s
+   --  system.exceptions_debug%b
+   --  system.storage_elements%s
+   --  system.storage_elements%b
+   --  system.stack_checking%s
+   --  system.stack_checking%b
+   --  system.traceback_entries%s
+   --  system.traceback_entries%b
+   --  ada.exceptions%s
+   --  system.soft_links%s
+   --  system.wch_con%s
+   --  system.wch_con%b
+   --  system.wch_cnv%s
+   --  system.wch_jis%s
+   --  system.wch_jis%b
+   --  system.wch_cnv%b
+   --  system.wch_stw%s
+   --  system.wch_stw%b
+   --  ada.exceptions.last_chance_handler%s
+   --  ada.exceptions.last_chance_handler%b
+   --  ada.exceptions.traceback%s
+   --  system.address_image%s
+   --  system.exception_table%s
+   --  system.exception_table%b
+   --  system.exceptions%s
+   --  system.exceptions%b
+   --  system.exceptions.machine%s
+   --  system.memory%s
+   --  system.memory%b
+   --  system.standard_library%b
+   --  system.secondary_stack%s
+   --  system.soft_links%b
+   --  system.secondary_stack%b
+   --  system.address_image%b
+   --  ada.exceptions.traceback%b
+   --  system.traceback%s
+   --  system.traceback%b
+   --  system.traceback.symbolic%s
+   --  system.traceback.symbolic%b
+   --  ada.exceptions%b
+   --  main%b
+   --  END ELABORATION ORDER
+
+
+end ada_main;
--- testsuite/tests/basic_tests/src/tc_read_partitions.adb
+++ testsuite/tests/basic_tests/src/tc_read_partitions.adb
@@ -1,5 +1,15 @@
-function TC_Read_Partitions return Integer is
+with AUnit.Assertions; use AUnit.Assertions;
+
+procedure TC_Read_Partitions is
+
+   procedure Run_Test (T : in Integer) is
+   begin
+      Assert (T = 1, "Addition is incorrect");
+   end Run_Test;
+
 begin
-   return 1;
+
+   Run_Test (1);
 
 end TC_Read_Partitions;
+
